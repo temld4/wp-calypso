9fdcaf22ef5d152d35ccc2799bcde2ab96c1e06a
John Godley <johng75@gmail.com>
2017-06-22 15:42:37 +0100

    Add Google Photos to editor media dropdown
    Need to add it to the visual and HTML editor. We allow the media modal to take the `source` param from props, so we can pass the data from the dropdown menu

--- client/components/tinymce/plugins/insert-menu/menu-items.jsx
+++ client/components/tinymce/plugins/insert-menu/menu-items.jsx
@@ -1,8 +1,16 @@
+/**
+ * External dependencies
+ */
 import React from 'react';
-
 import Gridicon from 'gridicons';
 import i18n from 'i18n-calypso';
 
+/**
+ * Internal dependencies
+ */
+import config from 'config';
+
+/* eslint-disable wpcalypso/jsx-classname-namespace */
 const GridiconButton = ( { icon, label } ) => (
 	<div>
 		<Gridicon className="wpcom-insert-menu__menu-icon" icon={ icon } />
@@ -10,15 +18,24 @@ const GridiconButton = ( { icon, label } ) => (
 	</div>
 );
 
-export default [
-	{
-		name: 'insert_media_item',
-		item: <GridiconButton icon="add-image" label={ i18n.translate( 'Add Media' ) } />,
-		cmd: 'wpcomAddMedia'
-	},
-	{
-		name: 'insert_contact_form',
-		item: <GridiconButton icon="mention" label={ i18n.translate( 'Add Contact Form' ) } />,
-		cmd: 'wpcomContactForm'
-	}
-];
+const menuItems = [	{
+	name: 'insert_media_item',
+	item: <GridiconButton icon="add-image" label={ i18n.translate( 'Add Media' ) } />,
+	cmd: 'wpcomAddMedia'
+} ];
+
+if ( config.isEnabled( 'external-media' ) ) {
+	menuItems.push( {
+		name: 'insert_from_google',
+		item: <GridiconButton icon="add-image" label={ i18n.translate( 'Add from Google' ) } />,
+		cmd: 'googleAddMedia'
+	} );
+}
+
+menuItems.push( {
+	name: 'insert_contact_form',
+	item: <GridiconButton icon="mention" label={ i18n.translate( 'Add Contact Form' ) } />,
+	cmd: 'wpcomContactForm'
+} );
+
+export default menuItems;
