bd5feb9d42a97bfead579d1a2ca9da663c9dccc9
Justin Shreve <justin.shreve@gmail.com>
2017-06-22 11:39:59 -0700

    Add basic breadcrumbs

--- client/extensions/woocommerce/app/settings/shipping/index.js
+++ client/extensions/woocommerce/app/settings/shipping/index.js
@@ -3,19 +3,28 @@
  */
 import React, { PropTypes } from 'react';
 import classNames from 'classnames';
+import { connect } from 'react-redux';
+import { localize } from 'i18n-calypso';
 
 /**
  * Internal dependencies
  */
+import ActionHeader from 'woocommerce/components/action-header';
+import { getLink } from 'woocommerce/lib/nav-utils';
+import { getSelectedSiteWithFallback } from 'woocommerce/state/sites/selectors';
 import Main from 'components/main';
 import ShippingLabels from './shipping-labels';
 import ShippingOrigin from './shipping-origin';
 import ShippingPackageList from './shipping-package-list';
 import ShippingZoneList from './shipping-zone-list';
 
-const Shipping = ( { className } ) => {
+const Shipping = ( { className, site, translate } ) => {
+	const breadcrumbs = ( <span>
+		<a href={ getLink( '/store/:site/', site ) }>{ translate( 'Settings' ) }</a> &gt; { translate( 'Shipping' ) }
+	</span> );
 	return (
 		<Main className={ classNames( 'shipping', className ) }>
+			<ActionHeader breadcrumbs={ breadcrumbs } />
 			<ShippingOrigin />
 			<ShippingZoneList />
 			<ShippingLabels />
@@ -25,7 +34,17 @@ const Shipping = ( { className } ) => {
 };
 
 Shipping.propTypes = {
-	className: PropTypes.string
+	site: PropTypes.shape( {
+		slug: PropTypes.string,
+	} ),
+	className: PropTypes.string,
 };
 
-export default Shipping;
+function mapStateToProps( state ) {
+	const site = getSelectedSiteWithFallback( state );
+	return {
+		site,
+	};
+}
+
+export default connect( mapStateToProps )( localize( Shipping ) );
